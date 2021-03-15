1981474486ee7a52102bdabbfd79eb0f568aee82
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-15 11:42:44 -0300

    Coversor Milhas/Km

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+            string comando;
+            double troca, Km;
+            
+            Console.WriteLine();
+            Console.WriteLine("--- Conversor Milhas/Km ---");
+            Console.WriteLine("Digite um valor a ser convertido...");
+            Console.WriteLine();
+            comando = Console.ReadLine();
+            Console.WriteLine();
+
+            troca = Convert.ToDouble(comando);
+            Km = troca*1.609;
+
+            Console.Beep();
+            Console.WriteLine($"Conversão: {Km:N3} Km.");
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
